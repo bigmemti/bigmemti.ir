28c079a66760df4caa186ec15f1ab6d6b98e454d
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2025-03-26 15:09:45 +0330

    add some how

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,7 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::resource('contact', ContactController::class, ['except' => ['create']]);
+    Route::resource('contact', ContactController::class, ['except' => ['store']]);
 });
+
+Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
 
 require __DIR__.'/auth.php';
